03f74f91c8934389245c6508eb18d4a6164f1d56
suchao <persistsuchao@hotmail.com>
2019-08-12 11:51:02 +0800

    rocketmq  延迟消息 设置从那开始消费

---
+++ src/main/java/com/answer/producer/SyncProducer.java
@@ -0,0 +1,32 @@
+package com.answer.producer;
+
+import org.apache.rocketmq.client.producer.DefaultMQProducer;
+import org.apache.rocketmq.client.producer.SendResult;
+import org.apache.rocketmq.common.message.Message;
+import org.apache.rocketmq.remoting.common.RemotingHelper;
+
+/**
+ * @Author answer
+ * @Date 2019/8/12 11 44
+ */
+
+public class SyncProducer {
+    public static void main(String[] args) throws Exception {
+        //Instantiate with a producer group name.
+        DefaultMQProducer producer = new DefaultMQProducer("syncProducerGroup");
+        // Specify name server addresses.
+        producer.setNamesrvAddr("localhost:9876");
+        //Launch the instance.
+        producer.start();
+        for (int i = 0; i < 100; i++) {
+            //Create a message instance, specifying topic, tag and message body.
+            Message msg = new Message("TopicTest","TagA" ,("Hello RocketMQ " +i).getBytes(RemotingHelper.DEFAULT_CHARSET)
+            );
+            //Call send message to deliver message to one of brokers.
+            SendResult sendResult = producer.send(msg);
+            System.out.printf("%s%n", sendResult);
+        }
+        //Shut down once the producer instance is not longer in use.
+        producer.shutdown();
+    }
+}
--- src/test/java/com/answer/producer/DefaultProductTest.java
+++ src/test/java/com/answer/producer/DefaultProductTest.java
@@ -1,10 +1,15 @@
 package com.answer.producer;
 
+import org.apache.rocketmq.client.consumer.DefaultMQPushConsumer;
+import org.apache.rocketmq.client.consumer.listener.ConsumeConcurrentlyStatus;
+import org.apache.rocketmq.client.consumer.listener.MessageListenerConcurrently;
 import org.apache.rocketmq.client.exception.MQBrokerException;
 import org.apache.rocketmq.client.exception.MQClientException;
 import org.apache.rocketmq.client.producer.DefaultMQProducer;
 import org.apache.rocketmq.client.producer.SendResult;
+import org.apache.rocketmq.common.consumer.ConsumeFromWhere;
 import org.apache.rocketmq.common.message.Message;
+import org.apache.rocketmq.common.message.MessageExt;
 import org.apache.rocketmq.remoting.exception.RemotingException;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -13,6 +18,8 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.Date;
 
 /**
  * @descreption
@@ -25,15 +32,47 @@ public class DefaultProductTest {
 
     private static final Logger logger = LoggerFactory.getLogger(DefaultProductTest.class);
 
-    @Autowired
-    private DefaultMQProducer producer;
+
 
     @Test
     public void send() throws InterruptedException, RemotingException, MQClientException, MQBrokerException {
-        String msg = "demo producer message";
+        DefaultMQProducer producer = new DefaultMQProducer("testProducerGroup");
+        producer.setNamesrvAddr("localhost:9876");
+        producer.start();
         logger.info("发送消息");
-        Message message = new Message("TestTopic1", "", msg.getBytes());
-        SendResult sendResult = producer.send(message);
-        logger.info("消息发送响应信息："+sendResult.toString());
+        for (int i = 0; i < 20; i++) {
+            String msg = "demo producer message " + i;
+            Message message = new Message("TestTopic1", "", msg.getBytes());
+            SendResult sendResult = producer.send(message);
+            logger.info("消息发送响应信息："+sendResult.toString());
+        }
+    }
+
+    @Test
+    public void consume() throws MQClientException {
+        DefaultMQPushConsumer consumer = new DefaultMQPushConsumer("testConsumerGroup");
+        consumer.setNamesrvAddr("localhost:9876");
+        consumer.subscribe("TestTopic1" ,"*");
+        consumer.setConsumeFromWhere(ConsumeFromWhere.CONSUME_FROM_FIRST_OFFSET);
+        consumer.registerMessageListener((MessageListenerConcurrently) (list, context)->{
+            try {
+                for (MessageExt messageExt : list) {
+                    String messageBody = new String(messageExt.getBody());
+                    System.out.println( new Date().toString() + "  消费响应:"+ messageExt.getMsgId() +", MsgBody:" + messageBody);
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+                return ConsumeConcurrentlyStatus.RECONSUME_LATER;
+            }
+            return  ConsumeConcurrentlyStatus.CONSUME_SUCCESS;
+        });
+//        consumer.registerMessageListener((MessageListenerConcurrently)(msgs , context)->{
+//            for (MessageExt msg : msgs) {
+//                String message = new String(msg.getBody());
+//                System.out.println("消费消息 " + message);
+//            }
+//            return ConsumeConcurrentlyStatus.CONSUME_SUCCESS;
+//        });
+        consumer.start();
     }
 }
