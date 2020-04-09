77479ee4b731361bbaecc88aa51420d63d6bcb79
suchao <suchao@01zhuanche.com>
2020-04-09 19:31:23 +0800

    test

--- src/main/java/com/answer/boot/consumer/ConsumerService.java
+++ src/main/java/com/answer/boot/consumer/ConsumerService.java
@@ -14,6 +14,7 @@ import java.util.List;
  */
 @Component
 public class ConsumerService {
+
     @EventListener(condition = "#event.msgs[0].topic=='user-topic' &&  #event.msgs[0].tags=='white'")
     public void rocketMqMsgListener(MessageEvent event) {
         try {
